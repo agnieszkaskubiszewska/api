c2013a82273cb406446149d37ff42e30517c1362
Agnieszka Skubiszewska <agnieszkaskubiszewska@MacBook-Pro-Agnieszka.local>
2023-10-24 12:08:23 +0200

    yaml update

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     reporters: [
       'default',
       process.env.GITHUB_ACTIONS === 'true'
-        ? ['jest-html-reporter', { outputPath: 'test-report.html' }]
+        ? ['jest-html-reporter', { outputPath: 'test-reports/test-report.html' }]
         : null, 
     ].filter(Boolean),
   };
